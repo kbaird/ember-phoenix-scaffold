5a2b55b293197319ca7ba0c2f1bdb812757906e8
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-09-28 09:14:24 -0700

    http://jsonapi.org/format/ specifies plural type - comply with that

--- test/controllers/todo_controller_test.exs
+++ test/controllers/todo_controller_test.exs
@@ -45,7 +45,7 @@ defmodule EmberPhoenixScaffold.TodoControllerTest do
     conn = post conn, todo_path(conn, :create), %{
       "meta" => %{},
       "data" => %{
-        "type" => "todo",
+        "type" => "todos",
         "attributes" => @valid_attrs,
         "relationships" => relationships
       }
@@ -59,7 +59,7 @@ defmodule EmberPhoenixScaffold.TodoControllerTest do
     conn = post conn, todo_path(conn, :create), %{
       "meta" => %{},
       "data" => %{
-        "type" => "todo",
+        "type" => "todos",
         "attributes" => @invalid_attrs,
         "relationships" => relationships
       }
@@ -73,7 +73,7 @@ defmodule EmberPhoenixScaffold.TodoControllerTest do
     conn = put conn, todo_path(conn, :update, todo), %{
       "meta" => %{},
       "data" => %{
-        "type" => "todo",
+        "type" => "todos",
         "id" => todo.id,
         "attributes" => @valid_attrs,
         "relationships" => relationships
@@ -89,7 +89,7 @@ defmodule EmberPhoenixScaffold.TodoControllerTest do
     conn = put conn, todo_path(conn, :update, todo), %{
       "meta" => %{},
       "data" => %{
-        "type" => "todo",
+        "type" => "todos",
         "id" => todo.id,
         "attributes" => @invalid_attrs,
         "relationships" => relationships
