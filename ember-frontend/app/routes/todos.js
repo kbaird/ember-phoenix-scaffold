206d420b2f05c91c9c511a0ec34ee6c7818fc9e7
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-09-28 15:45:38 -0700

    Add todos-related files to ember-frontend

---
+++ ember-frontend/app/routes/todos.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+let todos = [
+  {
+    "id": "1",
+    "inserted-at": "2016-09-28T15:02:23",
+    "is-completed": true,
+    "title": "Create the Phoenix App",
+    "updated-at": "2016-09-28T15:02:23"
+  },
+  {
+    "id": "2",
+    "inserted-at": "2016-09-28T15:02:23",
+    "is-completed": false,
+    "title": "Prepare the Ember App",
+    "updated-at": "2016-09-28T15:02:23"
+  },
+  {
+    "id": "3",
+    "inserted-at": "2016-09-28T15:02:23",
+    "is-completed": false,
+    "title": "Ensure the Apps Work Together",
+    "updated-at": "2016-09-28T17:25:51"
+  }
+];
+
+export default Ember.Route.extend({
+  model() { return todos; }
+});
---
+++ ember-frontend/tests/integration/components/todos-list-test.js
@@ -0,0 +1,22 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('todos-list', 'Integration | Component | todos list', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{todos-list}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{todos-list title="List of Todos" todos=['I am a todo']}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'List of Todos');
+});
